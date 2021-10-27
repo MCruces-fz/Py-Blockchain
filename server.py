508b365b87cc65ed5f3f3bb829e9d09a765e9a6a
MCruces-fz <mcsquared.fz@gmail.com>
2021-10-27 13:43:07 +0200

    Add function to compute hash from block

--- server.py
+++ server.py
@@ -1,5 +1,19 @@
+from hashlib import sha256
+import json
+
 class Block:
     def __init__(self, index, transactions, timestamp):
         self.index = [] 
         self.transactions = transactions 
         self.timestamp = timestamp
+
+ 
+def compute_hash(block): 
+    """ 
+    Function to create hash from a block
+
+    :param block: json object
+    :return: sha256 string
+    """ 
+    block_string = json.dumps(block, sort_keys=True) 
+    return sha256(block_string.encode()).hexdigest()
