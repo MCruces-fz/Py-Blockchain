0362d8fb20d658ed983ae5cd5861c4543d762342
MCruces-fz <mcsquared.fz@gmail.com>
2021-10-28 13:45:59 +0200

    Create full Block class with hash method

--- server.py
+++ server.py
@@ -1,19 +1,21 @@
 from hashlib import sha256
 import json
 
+
 class Block:
-    def __init__(self, index, transactions, timestamp):
-        self.index = [] 
-        self.transactions = transactions 
+    def __init__(self, index, transactions, timestamp, previous_hash, nonce=0):
+        self.index = index
+        self.transactions = transactions
         self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.nonce = nonce
 
- 
-def compute_hash(block): 
-    """ 
-    Function to create hash from a block
+    def compute_hash(self):
+        """ 
+        Function to create hash from a block
 
-    :param block: json object
-    :return: sha256 string
-    """ 
-    block_string = json.dumps(block, sort_keys=True) 
-    return sha256(block_string.encode()).hexdigest()
+        :param block: json object
+        :return: sha256 string
+        """ 
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(block_string.encode()).hexdigest()
